56850cbb24adefc12a13f7874447880467db823b
huli <huli@eitschei.com>
2017-07-28 22:18:17 +0200

    Lesser segments, more calmness.

--- js/code.js
+++ js/code.js
@@ -1,16 +1,20 @@
  
-var oldWidth = 0
-  function render(){
-    if (oldWidth == innerWidth) return
-    oldWidth = innerWidth;
 
-    d3.graphScroll()
-        .sections(d3.selectAll('#sections > div'))
-        .on('active', function(i)
-            { 
-                console.log(i + 'th section active') 
-            });
-  }
-  render()
 
-  d3.select(window).on('resize', render)
+    var oldWidth = 0
+    function render(){
+        if (oldWidth == innerWidth) return
+        oldWidth = innerWidth;
+
+        d3.graphScroll()
+            .sections(d3.selectAll('#sections > div'))
+            .on('active', function(i)
+                { 
+                    console.log(i + 'th section active') ;
+                });
+
+
+    }
+    render()
+
+    d3.select(window).on('resize', render)
