353a2b7ff6be7fd6869ea5104f99a22223852883
huli <huli@eitschei.com>
2017-08-11 15:03:05 +0200

    Fixed the jumping when changing absolute to relative.

--- js/code.js
+++ js/code.js
@@ -13,24 +13,49 @@ function render(){
         .on('active', function(i)
             { 
                 console.log(i + 'th section active') ;
+                if(i == 4)
+                {
+                    fixBubble();
+                }
             });
+}
 
+function fixBubble()
+{
+    var div_element = "#graph-bubble-chart";
+    var div_top = d3.select(div_element).node().getBoundingClientRect();
+    
+    d3.select(div_element)
+        .style("top", div_top.top + "px")        
+        .style("left", div_top.left + "px")
+        .style("position", "fixed");
 
+    hideOthers(div_element);
+}
+
+function hideOthers(nottohide)
+{
+    
 }
 
 
 function draw_map(geo_data)
 {
+    var margin = 75,
+            width = 1920 -margin,
+            height = 800 -margin;
+            
     var svg = d3.select("#graph-introduction")
                 .append("svg")
-                .attr("width", "100%")
-                .attr("height", "100%")
+                .attr("width", width + margin)
+                .attr("height", height + margin)
                 .append('g')
                 .attr('class', 'map');
 
+
     var projection = d3.geoMercator()
                     .scale(220)
-                    .translate([900 / 2.5, 700 / 3]);
+                    .translate([width / 2.5, height / 2]);
 
     var path = d3.geoPath().projection(projection);
 
